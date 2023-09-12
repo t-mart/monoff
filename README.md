dd2a647cf823b3dab6143c7e1034e86957201072
Tim Martin <tim@timmart.in>
2023-09-12 01:39:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# ![monoff](docs/monoff.png) monoff - Turn off your monitors on Windows
+
+monoff is a Windows program that turns off/sleeps your monitors -- that's it. When you
+move your mouse or press a key on the keyboard, they'll turn back on again.
+
+There's no magic to it. It works the same way as if you'd left your computer
+inactive for however many minutes.
+
+You can also specify a delay before the monitors turn off, so that you can
+safely remove your hand from your mouse, for example.
+
+I wrote this program because I purchased some monitors whose power switches
+were hard to reach. Now, I can just click an icon in my taskbar.
+
+This program is heavily inspired by [lcdoff-rs](https://github.com/Gekkio/lcdoff-rs) (and its partner [article](https://gekkio.fi/blog/2014/calling-win32-api-with-rust-ffi/)), but with a few differences:
+
+- uses the [`windows`](https://crates.io/crates/windows) crate, which is a
+  little easier/idiomatic to work with
+- accepts a delay parameter, the amount of time to wait before powering the monitors off
+- has a representative icon
+
+## Installation
+
+### From GitHub
+
+Download `monoff.exe` from the [latest
+release](https://github.com/t-mart/monoff/releases/latest) and put it somewhere,
+such as on your Desktop.
+
+### Cargo
+
+Run:
+
+```shell
+cargo install --git https://github.com/t-mart/monoff
+```
+
+## Making it a taskbar icon
+
+As normal, just drag the application to your taskbar. This will make a shortcut:
+
+![add to taskbar](/docs/add-to-taskbar.gif)
+
+From here, you can just click to icon to turn off your monitors.
+
+If you want to add a delay to this shortcut, edit the `Target` value its properties:
+
+![edit shortcut arguments](/docs/shortcut-arguments.gif)
+
+## Usage examples
+
+- Turn off your monitors immediately:
+
+  ```shell
+  monoff.exe
+  ```
+
+- Turn them off after a delay of 2 seconds:
+
+  ```shell
+  monoff.exe --delay 2000
+  ```
+
+- See help:
+
+  ```shell
+  monoff.exe --help
+  ```
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+#![windows_subsystem = "windows"]
+
+use std::thread::sleep;
+use std::time::Duration;
+
+use clap::Parser;
+use windows::{
+    core::*, Win32::Foundation::*, Win32::System::Console::*,
+    Win32::System::LibraryLoader::GetModuleHandleW,
+    Win32::UI::WindowsAndMessaging::*,
+};
+
+const DEFAULT_DELAY: u16 = 50;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of millseconds to delay before turning off the monitors
+    #[arg(
+        short,
+        long, 
+        default_value_t = DEFAULT_DELAY,
+        value_parser = clap::value_parser!(u16).range(0..))
+    ]
+    delay: u16,
+}
+
+fn main() -> Result<()> {
+    // so stdin/out/err work
+    unsafe {
+        AttachConsole(ATTACH_PARENT_PROCESS)?;
+    }
+
+    let args = Args::parse();
+    let sleep_duration = Duration::from_millis(args.delay.into());
+    sleep(sleep_duration);
+
+    unsafe {
+        let instance = GetModuleHandleW(None)?;
+
+        let window_class = w!("monoff");
+
+        let wc = WNDCLASSW {
+            hInstance: instance.into(),
+            lpszClassName: window_class,
+            lpfnWndProc: Some(wndproc),
+            ..Default::default()
+        };
+
+        RegisterClassW(&wc);
+
+        let window = CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            window_class,
+            PCWSTR::null(),
+            WS_OVERLAPPED,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            None,
+            None,
+            instance,
+            None,
+        );
+
+        SendNotifyMessageW(
+            window,
+            WM_SYSCOMMAND,
+            WPARAM(SC_MONITORPOWER as usize),
+            LPARAM(2),
+        )
+    }
+}
+
+extern "system" fn wndproc(
+    window: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    // this function just passes through to DefWindowProcW. the lpfnWndProc
+    // field of WNDCLASSW doesn't allow DefWindowProcW though, so we create a
+    // new function pointer that does. this might be a bug. See:
+    //   - https://github.com/microsoft/windows-rs/issues/711
+    //   - https://github.com/microsoft/windows-rs/issues/2556
+    unsafe { DefWindowProcW(window, message, wparam, lparam) }
+}
