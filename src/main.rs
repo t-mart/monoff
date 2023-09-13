795354250526f529aa50eb97ecd92f60d1685180
Tim Martin <tim@timmart.in>
2023-09-13 05:00:45 -0500

    Rework error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // prevent a console window from flashing
 #![windows_subsystem = "windows"]
 
+use std::process::ExitCode;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -9,11 +10,12 @@ use windows::{
     core::*,
     Win32::{
         Foundation::*,
-        System::{Console::*, LibraryLoader::GetModuleHandleW},
+        System::{Console::*, LibraryLoader::*},
         UI::WindowsAndMessaging::*,
     },
 };
 
+const APPLICATION_NAME: &str = "monoff";
 const DEFAULT_DELAY: u16 = 50;
 
 #[derive(Parser, Debug)]
@@ -29,25 +31,27 @@ struct Args {
     delay: u16,
 }
 
-/// Attach to the parent console so that stdin/stdout/stderr work.
-fn attach_console() -> Result<()> {
+/// Try to attach to the parent console (which means stdout/err are can be
+/// used), or return an error. This will be an error if the parent doesn't have
+/// a console, like if the program is double-clicked in explorer, run from the
+/// Run dialog (Win+R), etc.
+fn try_attach_console() -> Result<()> {
     unsafe { AttachConsole(ATTACH_PARENT_PROCESS) }
 }
 
 /// Turn off the monitors.
-///
-/// This function creates a window and sends a message to it to turn off the
-/// monitors. The window is never shown, and is destroyed when the program
-/// exits.
 fn turn_off_monitors() -> Result<()> {
-    let window_class_name = w!("monoff");
+    // This function creates an invisible window that will accept a message to
+    // turn off the monitors. Then, we send that message. (If this sounds
+    // strange, that's just how Windows works.)
+    let window_class_name = HSTRING::from(APPLICATION_NAME);
 
     let instance = unsafe { GetModuleHandleW(None)? };
 
     let window_class = WNDCLASSW {
         hInstance: instance.into(),
-        lpszClassName: window_class_name,
-        lpfnWndProc: Some(wndproc),
+        lpszClassName: PCWSTR(window_class_name.as_wide().as_ptr()),
+        lpfnWndProc: Some(window_proc),
         ..Default::default()
     };
 
@@ -56,7 +60,7 @@ fn turn_off_monitors() -> Result<()> {
     let window = unsafe {
         CreateWindowExW(
             WINDOW_EX_STYLE::default(),
-            window_class_name,
+            &window_class_name,
             PCWSTR::null(),
             WS_OVERLAPPED,
             CW_USEDEFAULT,
@@ -80,31 +84,85 @@ fn turn_off_monitors() -> Result<()> {
     }
 }
 
-/// this function processes messages that our window receives. we just do the
-/// default and passthrough to DefWindowProcW.
-///
-/// why don't we just use DefWindowProcW directly, you ask? because
-/// DefWindowProcW is unsafe, but the interface for the lpfnWndProc field of the
-/// WNDCLASSW wants a safe function. so we have to wrap it in a safe function.
-/// this might be a bug, at least questionable design. see discussion here:
-///   - https://github.com/microsoft/windows-rs/issues/711
-///   - https://github.com/microsoft/windows-rs/issues/2556
-extern "system" fn wndproc(
+/// Handler for messages sent to our window.
+extern "system" fn window_proc(
     window: HWND,
     message: u32,
     wparam: WPARAM,
     lparam: LPARAM,
 ) -> LRESULT {
+    // As far as handling monitor power system command, we don't know how to do
+    // it, but DefWindowProcW does, so we just passthrough to that.
+    //
+    // Why don't we just use DefWindowProcW directly, you ask? Because
+    // DefWindowProcW is unsafe, but the interface for the lpfnWndProc field of
+    // the WNDCLASSW wants a safe function. So, essentially, this is just a
+    // wrapper function that changes the "safety" from unsafe to safe. This
+    // might be a bug, at least questionable design. See discussion here:
+    //   - https://github.com/microsoft/windows-rs/issues/711
+    //   - https://github.com/microsoft/windows-rs/issues/2556
     unsafe { DefWindowProcW(window, message, wparam, lparam) }
 }
 
-fn main() -> Result<()> {
-    attach_console()?;
+/// Show a message box. If error, the message box will have an error icon,
+/// otherwise it will have an information icon.
+fn show_message_box(text: &str, caption: &str, error: bool) -> () {
+    let utype = if error {
+        MB_ICONERROR | MB_OK
+    } else {
+        MB_ICONINFORMATION | MB_OK
+    };
 
-    // parse args and sleep for delay
-    let args = Args::parse();
-    let sleep_duration = Duration::from_millis(args.delay.into());
-    sleep(sleep_duration);
+    unsafe {
+        let _ = MessageBoxW(
+            None,
+            &HSTRING::from(text),
+            &HSTRING::from(caption),
+            utype,
+        );
+    }
+}
 
-    turn_off_monitors()
+fn main() -> ExitCode {
+    // this result dictates whether we will show output in the console or in a
+    // message box
+    let attach_con_result = try_attach_console();
+
+    let args = match Args::try_parse() {
+        Ok(args) => args,
+        Err(err) => {
+            // --help, for example, will have an exit_code() == 0
+            let exit_code_is_error = err.exit_code() != 0;
+            if attach_con_result.is_err() {
+                show_message_box(
+                    &err.to_string(),
+                    APPLICATION_NAME,
+                    exit_code_is_error,
+                );
+            } else {
+                let _ = err.print();
+            }
+            return match exit_code_is_error {
+                true => ExitCode::FAILURE,
+                false => ExitCode::SUCCESS,
+            };
+        }
+    };
+
+    let delay_duration = Duration::from_millis(args.delay.into());
+    sleep(delay_duration);
+
+    match turn_off_monitors() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            let error_message =
+                format!("Error turning off monitors: {}", err.to_string());
+            if attach_con_result.is_err() {
+                show_message_box(&error_message, APPLICATION_NAME, true);
+            } else {
+                eprintln!("{}", error_message);
+            }
+            ExitCode::FAILURE
+        }
+    }
 }
