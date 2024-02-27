18ad7e7937fb4a58e7e4f4b9c9faaf3a2689dba4
Tim Martin <tim@timmart.in>
2024-02-27 16:09:11 -0600

    Rearchitect error/message handling

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@
 // prevent a console window from flashing
 #![windows_subsystem = "windows"]
 
-use std::process::ExitCode;
-use std::thread::sleep;
-use std::time::Duration;
-
 use clap::Parser;
+use std::{ops::Deref, process::ExitCode, thread::sleep, time::Duration};
 use windows::{
     core::{Result, HSTRING, PCWSTR},
     Win32::{
@@ -43,19 +40,11 @@ struct Args {
     delay: u16,
 }
 
-/// Try to attach to the parent console (which means stdout/err are can be
-/// used), or return an error. This will be an error if the parent doesn't have
-/// a console, like if the program is double-clicked in explorer, run from the
-/// Run dialog (Win+R), etc.
-fn try_attach_console() -> Result<()> {
-    unsafe { AttachConsole(ATTACH_PARENT_PROCESS) }
-}
-
 /// Turn off the monitors.
 fn turn_off_monitors() -> Result<()> {
     // This function creates an invisible window that will accept a message to
     // turn off the monitors. Then, we send that message. (If this sounds
-    // strange, that's just how Windows works.)
+    // strange, then know that this is just how Windows works.)
     let window_class_name = HSTRING::from(APPLICATION_NAME);
 
     let instance = unsafe { GetModuleHandleW(None)? };
@@ -116,54 +105,138 @@ extern "system" fn window_proc(
     unsafe { DefWindowProcW(window, message, wparam, lparam) }
 }
 
-/// Show a message box with text and a title. If error, the message
-/// box will have an error icon, otherwise it will have an information icon.
-fn show_message_box(text: &str, title: &str, error: bool) {
-    let utype = if error {
-        MB_ICONERROR | MB_OK
-    } else {
-        MB_ICONINFORMATION | MB_OK
-    };
+/// A wrapper for [`clap::Error`] that better exposes the kind of error.
+enum ClapError<'err> {
+    /// An error that should be printed to stderr
+    ParseError(&'err clap::Error),
+    /// An error that should be printed to stdout, such as when the user runs with `--version` or
+    /// `--help` options.
+    Informational(&'err clap::Error),
+}
 
-    unsafe {
-        let _ = MessageBoxW(
-            None,
-            &HSTRING::from(text),
-            &HSTRING::from(title),
-            utype,
-        );
+impl ClapError<'_> {
+    fn exit_code(&self) -> ExitCode {
+        match self {
+            Self::ParseError(_) => ExitCode::FAILURE,
+            Self::Informational(_) => ExitCode::SUCCESS,
+        }
+    }
+
+    fn show(&self, message_console: &MessageConsole) {
+        match message_console {
+            MessageConsole::Textual => {
+                // unwrap here because there's nothing we can really do to recover here. this is
+                // would be an error with the error handling itself.
+                self.print().unwrap();
+            }
+
+            MessageConsole::Graphical => {
+                message_console.show_message(
+                    &self.to_string(),
+                    matches!(self, ClapError::ParseError(_)),
+                );
+            }
+        }
+    }
+}
+
+impl Deref for ClapError<'_> {
+    type Target = clap::Error;
+
+    /// Easy access to the underlying error
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::ParseError(err) | Self::Informational(err) => err,
+        }
+    }
+}
+
+impl<'err> From<&'err clap::Error> for ClapError<'err> {
+    /// Create a [`ClapError`] from a [`clap::Error`].
+    fn from(err: &'err clap::Error) -> Self {
+        // To differentiate between parse and information errors, we check if the error is destined
+        // to stdout or stderr. Unfortunately, this it the best way way I could find to do this with
+        // clap's API.
+        if err.use_stderr() {
+            Self::ParseError(err)
+        } else {
+            Self::Informational(err)
+        }
+    }
+}
+
+/// A "console" to display messages to the user
+enum MessageConsole {
+    /// A console that has stdout/err. This should be chosen when the user is running the program
+    /// from a console to keep messaging "in-band".
+    Textual,
+    /// A console that displays messages in a message box. This should be chosen when the user is
+    /// running the program from "explorer"-like interfaces, such as double-clicking an icon.
+    Graphical,
+}
+
+impl MessageConsole {
+    /// Try to attach to the parent console (which means stdout/err are can be used), or return an
+    /// error. This will be an error if the parent doesn't have a console, like if the program is
+    /// double-clicked in explorer, run from the Run dialog (Win+R), etc.
+    ///
+    /// First attempt to attach to a textual console (that has stdout/err) if available, otherwise
+    /// use a "graphical" console, which displays messages with a explorer message box.
+    fn attach_to_available() -> MessageConsole {
+        let res = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
+        match res {
+            Ok(()) => Self::Textual,
+            Err(_) => Self::Graphical,
+        }
+    }
+
+    fn show_message(&self, message: &str, is_error: bool) {
+        match self {
+            Self::Textual => {
+                if is_error {
+                    eprintln!("{message}");
+                } else {
+                    println!("{message}");
+                }
+            }
+            Self::Graphical => {
+                Self::show_message_box(message, APPLICATION_NAME, is_error);
+            }
+        }
+    }
+
+    /// Show a message box with text and a title. If error, the message
+    /// box will have an error icon, otherwise it will have an information icon.
+    fn show_message_box(text: &str, title: &str, error: bool) {
+        let messagebox_style = if error {
+            MB_ICONERROR | MB_OK
+        } else {
+            MB_ICONINFORMATION | MB_OK
+        };
+
+        unsafe {
+            let _ = MessageBoxW(
+                None,
+                &HSTRING::from(text),
+                &HSTRING::from(title),
+                messagebox_style,
+            );
+        }
     }
 }
 
 fn main() -> ExitCode {
     // this result dictates whether we will show output in the console or in a
     // message box
-    let attach_con_result = try_attach_console();
-    let has_console = attach_con_result.is_ok();
+    let message_console = MessageConsole::attach_to_available();
 
     let args = match Args::try_parse() {
         Ok(args) => args,
 
-        // little bit of a misnomer, but an Err can indicate --help or
-        // --version, which are not really errors, but instead indicate we
-        // shouldn't do our normal application logic
         Err(err) => {
-            // figure out if we're in one of those non-error scenarios
-            let is_parse_error = err.use_stderr();
-            if has_console {
-                let _ = err.print();
-            } else {
-                show_message_box(
-                    &err.to_string(),
-                    APPLICATION_NAME,
-                    is_parse_error,
-                );
-            }
-            return if is_parse_error {
-                ExitCode::FAILURE
-            } else {
-                ExitCode::SUCCESS
-            };
+            let clap_error = ClapError::from(&err);
+            clap_error.show(&message_console);
+            return clap_error.exit_code();
         }
     };
 
@@ -174,11 +247,7 @@ fn main() -> ExitCode {
         Ok(()) => ExitCode::SUCCESS,
         Err(err) => {
             let error_message = format!("Error turning off monitors: {err}");
-            if has_console {
-                eprintln!("{error_message}");
-            } else {
-                show_message_box(&error_message, APPLICATION_NAME, true);
-            }
+            message_console.show_message(&error_message, true);
             ExitCode::FAILURE
         }
     }
